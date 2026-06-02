1a8629934d2ff7893f22b665b726fb72fd16fe55
strahlenguy <pokemoy999@gmail.com>
2026-06-01 18:56:05 -0600

    checkout fixes

--- app/admin/obras/[id]/page.tsx
+++ app/admin/obras/[id]/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { use, useEffect, useState, FormEvent } from "react";
+
+export const dynamic = 'force-dynamic';
 import { useRouter } from "next/navigation";
 import { COLLECTIONS, CATEGORIES } from "@/lib/constants";
 
